0b0b0d34f6e008e2212d1e37075cda6295fa8682
fangyeqi-lenovau <yeqifang@outlook.com>
2025-06-08 04:14:57 +0800

    add config

--- pyfast-multiprocess-semi.py
+++ pyfast-multiprocess-semi.py
@@ -1,16 +1,18 @@
 import os
 import numpy as np
 import pyfstat
+import config
 # make sure to put these after the pyfstat import, to not break notebook inline plots
 import matplotlib.pyplot as plt
 # %matplotlib inline
+from utils import plot_grid_vs_samples, plot_2F_scatter
+
 
 import concurrent.futures
 # flip this switch for a more expensive 4D (F0,F1,Alpha,Delta) run
 # instead of just (F0,F1)
 # (still only a few minutes on current laptops)
-sky = False
-plot = False
+
 # log = 
 
 
@@ -18,148 +20,39 @@ plot = False
 label = "PyFstatExampleSimpleMCMCvsGridComparisonSemi"
 outdir = os.path.join("PyFstat_example_data", label)
 logger = pyfstat.set_up_logger(label=label, outdir=outdir, log_level="WARNING")
-if sky:
+if config.sky:
     outdir += "AlphaDelta"
 printout = False
 # parameters for the data set to generate
-tstart = 1000000000
-duration = 120 * 86400
-T_coh = 15 * 86400  # coherence time for the MCMC
-nsegs = int(duration / T_coh)  # number of segments for the MCMC
-Tsft = 1800
-detectors = "H1,L1"
-sqrtSX = 1e-22
+
 
 # parameters for injected signals
-inj = {
-    "tref": tstart,
-    "F0": 30.0,
-    "F1": -1e-10,
-    "F2": 0,
-    "Alpha": 0.5,
-    "Delta": 1,
-    "h0": 1 * sqrtSX,
-    "cosi": 1.0,
-}
-
-# latex-formatted plotting labels
-labels = {
-    "F0": "$f$ [Hz]",
-    "F1": "$\\dot{f}$ [Hz/s]",
-    "2F": "$2\\mathcal{F}$",
-    "Alpha": "$\\alpha$",
-    "Delta": "$\\delta$",
-}
-labels["max2F"] = "$\\max\\,$" + labels["2F"]
 
 # create SFT files
 logger.info("Generating SFTs with injected signal...")
 writer = pyfstat.Writer(
     label=label + "SimulatedSignal",
     outdir=outdir,
-    tstart=tstart,
-    duration=duration,
-    detectors=detectors,
-    sqrtSX=sqrtSX,
-    Tsft=Tsft,
-    **inj,
+    tstart=config.tstart,
+    duration=config.duration,
+    detectors=config.detectors,
+    sqrtSX=config.sqrtSX,
+    Tsft=config.Tsft,
+    **config.inj,
     Band=1,  # default band estimation would be too narrow for a wide grid/prior
 )
 writer.make_data()
 
 # set up square search grid with fixed (F0,F1) mismatch
 # and (optionally) some ad-hoc sky coverage
-mf = 0.15
-mf1 = 0.3
-mf2 = 0.003
-gamma1 = 8
-gamma2 = 20
-dF0 = np.sqrt(12 * mf) / (np.pi * T_coh)
-dF1 = np.sqrt(180 * mf1) / (np.pi * T_coh**2) / gamma1
-dF2 = np.sqrt(25200 * mf2) / (np.pi * T_coh**3) / gamma2
 
-
-DeltaF0 = 30 * dF0 # 500 
-DeltaF1 = 20 * dF1 # 200
-DeltaF2 = 10 * dF2 # 60
-print(DeltaF0, DeltaF1, DeltaF2)
-
-
-if sky:
-    # cover less range to keep runtime down
-    DeltaF0 /= 10
-    DeltaF1 /= 10
-    DeltaF2 /= 10
+print(config.DeltaF0, config.DeltaF1, config.DeltaF2)
     
-    
-zoom = {
-        "F0": [inj["F0"] - 10 * dF0, inj["F0"] + 10 * dF0],
-        "F1": [inj["F1"] - 5 * dF1, inj["F1"] + 5 * dF1],
-    }
 
-
-# some plotting helper functions
-def plot_grid_vs_samples(grid_res, mcmc_res, xkey, ykey):
-    """local plotting function to avoid code duplication in the 4D case"""
-    plt.plot(grid_res[xkey], grid_res[ykey], ".", label="grid")
-    plt.plot(mcmc_res[xkey], mcmc_res[ykey], ".", label="mcmc")
-    plt.plot(inj[xkey], inj[ykey], "*k", label="injection")
-    grid_maxidx = np.argmax(grid_res["twoF"])
-    mcmc_maxidx = np.argmax(mcmc_res["twoF"])
-    plt.plot(
-        grid_res[xkey][grid_maxidx],
-        grid_res[ykey][grid_maxidx],
-        "+g",
-        label=labels["max2F"] + "(grid)",
-    )
-    plt.plot(
-        mcmc_res[xkey][mcmc_maxidx],
-        mcmc_res[ykey][mcmc_maxidx],
-        "xm",
-        label=labels["max2F"] + "(mcmc)",
-    )
-    plt.xlabel(labels[xkey])
-    plt.ylabel(labels[ykey])
-    plt.legend()
-    plotfilename_base = os.path.join(outdir, "grid_vs_mcmc_{:s}{:s}".format(xkey, ykey))
-    plt.savefig(plotfilename_base + ".png")
-    if xkey == "F0" and ykey == "F1":
-        plt.xlim(zoom[xkey])
-        plt.ylim(zoom[ykey])
-        plt.savefig(plotfilename_base + "_zoom.png")
-    # plt.show()
-
-def plot_2F_scatter(res, label, xkey, ykey):
-    """local plotting function to avoid code duplication in the 4D case"""
-    markersize = 1 if label == "grid" else 0.5
-    plt.figure(figsize=(10, 6))
-    sc = plt.scatter(res[xkey], res[ykey], c=res["twoF"], s=markersize)
-    cb = plt.colorbar(sc)
-    plt.xlabel(labels[xkey])
-    plt.ylabel(labels[ykey])
-    cb.set_label(labels["2F"])
-    plt.title(label)
-    plt.plot(inj[xkey], inj[ykey], "*k", label="injection")
-    maxidx = np.argmax(res["twoF"])
-    plt.plot(
-        res[xkey][maxidx],
-        res[ykey][maxidx],
-        "+r",
-        label=labels["max2F"],
-    )
-    plt.legend(loc='upper right')
-    plotfilename_base = os.path.join(outdir, "{:s}_{:s}{:s}_2F".format(label, xkey, ykey))
-    plt.xlim([min(res[xkey]), max(res[xkey])])
-    plt.ylim([min(res[ykey]), max(res[ykey])])
-    plt.savefig(plotfilename_base + ".png")
-    # plt.show()
-
-
-numbers = 500
 mismatches = []
-F0s_random = np.random.uniform(-dF0, dF0, size=numbers)
-F1s_random = np.random.uniform(-dF1, dF1, size=numbers)
-F2s_random = np.random.uniform(-dF2, dF2, size=numbers)
+F0s_random = np.random.uniform(-config.dF0, config.dF0, size=config.numbers)
+F1s_random = np.random.uniform(-config.dF1, config.dF1, size=config.numbers)
+F2s_random = np.random.uniform(-config.dF2, config.dF2, size=config.numbers)
 
 
 def calculate_mismatch(i):
@@ -170,23 +63,22 @@ def calculate_mismatch(i):
     import os
     
 
-
-    F0s = [inj["F0"] - DeltaF0 / 2.0 + F0s_random[i], inj["F0"] + DeltaF0 / 2.0 + F0s_random[i], dF0]
-    F1s = [inj["F1"] - DeltaF1 / 2.0 + F1s_random[i], inj["F1"] + DeltaF1 / 2.0 + F1s_random[i], dF1]
-    F2s = [inj["F2"] - DeltaF2 / 2.0 + F2s_random[i], inj["F2"] + DeltaF2 / 2.0 + F2s_random[i], dF2]
+    F0s = [config.inj["F0"] - config.DeltaF0 / 2.0 + F0s_random[i], config.inj["F0"] + config.DeltaF0 / 2.0 + F0s_random[i], config.dF0]
+    F1s = [config.inj["F1"] - config.DeltaF1 / 2.0 + F1s_random[i], config.inj["F1"] + config.DeltaF1 / 2.0 + F1s_random[i], config.dF1]
+    F2s = [config.inj["F2"] - config.DeltaF2 / 2.0 + F2s_random[i], config.inj["F2"] + config.DeltaF2 / 2.0 + F2s_random[i], config.dF2]
     
     
     search_keys = ["F0", "F1", "F2"]  # only the ones that aren't 0-width
 
-    if sky:
+    if config.sky:
         dSky = 0.01  # rather coarse to keep runtime down
         DeltaSky = 10 * dSky
-        Alphas = [inj["Alpha"] - DeltaSky / 2.0, inj["Alpha"] + DeltaSky / 2.0, dSky]
-        Deltas = [inj["Delta"] - DeltaSky / 2.0, inj["Delta"] + DeltaSky / 2.0, dSky]
+        Alphas = [config.inj["Alpha"] - DeltaSky / 2.0, config.inj["Alpha"] + DeltaSky / 2.0, dSky]
+        Deltas = [config.inj["Delta"] - DeltaSky / 2.0, config.inj["Delta"] + DeltaSky / 2.0, dSky]
         search_keys += ["Alpha", "Delta"]
     else:
-        Alphas = [inj["Alpha"]]
-        Deltas = [inj["Delta"]]
+        Alphas = [config.inj["Alpha"]]
+        Deltas = [config.inj["Delta"]]
 
     search_keys_label = "".join(search_keys)
 
@@ -201,8 +93,8 @@ def calculate_mismatch(i):
         F2s=F2s,
         Alphas=Alphas,
         Deltas=Deltas,
-        tref=inj["tref"],
-        nsegs=nsegs,
+        tref=config.inj["tref"],
+        nsegs=config.nsegs,
     )
     gridsearch.run()
     gridsearch.print_max_twoF()
@@ -210,24 +102,24 @@ def calculate_mismatch(i):
 
 
     # do some plots of the GridSearch results
-    if not sky:  # this plotter can't currently deal with too large result arrays
+    if not config.sky:  # this plotter can't currently deal with too large result arrays
         logger.info("Plotting 1D 2F distributions...")
-        if plot:
+        if config.plot:
             for key in search_keys:
-                gridsearch.plot_1D(xkey=key, xlabel=labels[key], ylabel=labels["2F"])
+                gridsearch.plot_1D(xkey=key, xlabel=config.labels[key], ylabel=config.labels["2F"])
 
         logger.info("Making GridSearch {:s} corner plot...".format("-".join(search_keys)))
-        vals = [np.unique(gridsearch.data[key]) - inj[key] for key in search_keys]
+        vals = [np.unique(gridsearch.data[key]) - config.inj[key] for key in search_keys]
         twoF = gridsearch.data["twoF"].reshape([len(kval) for kval in vals])
         corner_labels = [
             "$f - f_0$ [Hz]",
             "$\\dot{f} - \\dot{f}_0$ [Hz/s]",
         ]
-        if sky:
+        if config.sky:
             corner_labels.append("$\\alpha - \\alpha_0$")
             corner_labels.append("$\\delta - \\delta_0$")
-        corner_labels.append(labels["2F"])
-        if plot:
+        corner_labels.append(config.labels["2F"])
+        if config.plot:
             gridcorner_fig, gridcorner_axes = pyfstat.gridcorner(
                 twoF, vals, projection="log_mean", labels=corner_labels,
                 whspace=0.1, factor=1.8
@@ -239,9 +131,9 @@ def calculate_mismatch(i):
 
     # we'll use the two local plotting functions defined above
     # to avoid code duplication in the sky case
-    if plot:
+    if config.plot:
         plot_2F_scatter(gridsearch.data, "grid", "F0", "F1")
-        if sky:
+        if config.sky:
             plot_2F_scatter(gridsearch.data, "grid", "Alpha", "Delta")
         
         
@@ -252,25 +144,26 @@ def calculate_mismatch(i):
 
 
     search_ranges = {
-        "F0":    [inj["F0"]],         # a single value ⇒ zero width,
-        "Alpha": [inj["Alpha"]],
-        "Delta": [inj["Delta"]],
+        "F0":    [config.inj["F0"]],         # a single value ⇒ zero width,
+        "Alpha": [config.inj["Alpha"]],
+        "Delta": [config.inj["Delta"]],
     }
+    
     fs = pyfstat.SemiCoherentSearch(
         label="MismatchTest",  # SemiCoherentSearch需要label
         outdir=outdir,         # 需要outdir
-        tref=inj["tref"],
-        nsegs=nsegs,              # 添加分段数
+        tref=config.inj["tref"],
+        nsegs=config.nsegs,              # 添加分段数
         sftfilepattern=writer.sftfilepath,
-        minStartTime=tstart,
-        maxStartTime=tstart + duration,
+        minStartTime=config.tstart,
+        maxStartTime=config.tstart + config.duration,
         search_ranges=search_ranges,
     )
 
     grid_res = gridsearch.data
 
     # template exactly at the injected parameters
-    inj_pars = {k: inj[k] for k in ("F0", "F1", "F2", "Alpha", "Delta")}
+    inj_pars = {k: config.inj[k] for k in ("F0", "F1", "F2", "Alpha", "Delta")}
 
     twoF_inj = fs.get_semicoherent_det_stat(params=inj_pars)
 
@@ -304,7 +197,7 @@ if __name__ == "__main__":
     # run the mismatch calculation in parallel
     with concurrent.futures.ProcessPoolExecutor(10) as executor:
         futures = []
-        for i in range(numbers):
+        for i in range(config.numbers):
             futures.append(executor.submit(calculate_mismatch, i))
         mismatches = [future.result() for future in concurrent.futures.as_completed(futures)]
 
@@ -326,39 +219,7 @@ if __name__ == "__main__":
     plt.show()
 
     
-    # rumtime
-    DeltaF0_fixed = 9.885590880794127e-06
-    DeltaF1_fixed = 3.481585082097677e-12
-    DeltaF2_fixed = 6.357202196709655e-19
-    
-    Nf0 = DeltaF0_fixed / dF0
-    Nf1 = DeltaF1_fixed / dF1
-    Nf2 = DeltaF2_fixed / dF2
-    
-    N_det = 2
-    N_coh = Nf0 * Nf1 * Nf2
-    
-    N_can = 0
-    
-    tau_Fbin = 6e-8
-    tau_fft = 3.3e-8
-    tau_spin = 7.5e-8
-    tau_bayes = 4.4e-8
-    tau_recalc = 0
-    
-    
-    ratio = 2
-    
-    R = 1
-    
-    N_inc = N_coh * gamma1 * gamma2
-    
-    tau_sumF = 7.28e-9 - 3.72e-10 * nsegs
-    
-    tau_RS = tau_Fbin + ratio * (tau_fft + R * tau_spin)
-    tau_total = nsegs * N_det * N_coh * tau_RS + nsegs * N_inc * tau_sumF + \
-        N_inc * tau_bayes + N_can * tau_recalc
-        
-    print("runtime: ", tau_total)
+    # rumtime    
+    print("runtime: ", config.tau_total)
     
     
--- utils.py
+++ utils.py
@@ -1,238 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import config
 
 
+zoom = {
+        "F0": [config.inj["F0"] - 10 * config.dF0, config.inj["F0"] + 10 * config.dF0],
+        "F1": [config.inj["F1"] - 5 * config.dF1, config.inj["F1"] + 5 * config.dF1],
+}
 
-def calculate_mismatch_from_fstats(twoF_signal, twoF_template):
-    """
-    Calculate mismatch from F-statistic values.
-    
-    This uses the relationship: mismatch ≈ 1 - (2F_template - 4)/(2F_signal - 4)
-    since 2F = 4 + SNR² for matched filtering.
-    """
-    # Convert 2F to SNR²: SNR² = 2F - 4
-    snr2_signal = twoF_signal - 4.0
-    snr2_template = twoF_template - 4.0
-    print(f"2F signal: {twoF_signal:.6f}, 2F template: {twoF_template:.6f}")
-    print(f"SNR² signal: {snr2_signal:.6f}, SNR² template: {snr2_template:.6f}")
-    # Mismatch = 1 - (recovered power / optimal power)
-    # if snr2_signal <= 0:
-    #     return np.nan
-    
-    mismatch = 1.0 - (snr2_template / snr2_signal)
-    return mismatch  # Mismatch should be non-negative
+# some plotting helper functions
+def plot_grid_vs_samples(grid_res, mcmc_res, xkey, ykey):
+    """local plotting function to avoid code duplication in the 4D case"""
+    plt.plot(grid_res[xkey], grid_res[ykey], ".", label="grid")
+    plt.plot(mcmc_res[xkey], mcmc_res[ykey], ".", label="mcmc")
+    plt.plot(config.inj[xkey], config.inj[ykey], "*k", label="injection")
+    grid_maxidx = np.argmax(grid_res["twoF"])
+    mcmc_maxidx = np.argmax(mcmc_res["twoF"])
+    plt.plot(
+        grid_res[xkey][grid_maxidx],
+        grid_res[ykey][grid_maxidx],
+        "+g",
+        label=config.labels["max2F"] + "(grid)",
+    )
+    plt.plot(
+        mcmc_res[xkey][mcmc_maxidx],
+        mcmc_res[ykey][mcmc_maxidx],
+        "xm",
+        label=config.labels["max2F"] + "(mcmc)",
+    )
+    plt.xlabel(config.labels[xkey])
+    plt.ylabel(config.labels[ykey])
+    plt.legend()
+    plotfilename_base = os.path.join(outdir, "grid_vs_mcmc_{:s}{:s}".format(xkey, ykey))
+    plt.savefig(plotfilename_base + ".png")
+    if xkey == "F0" and ykey == "F1":
+        plt.xlim(zoom[xkey])
+        plt.ylim(zoom[ykey])
+        plt.savefig(plotfilename_base + "_zoom.png")
+    # plt.show()
 
-
-
-def calculate_f0_f1_mismatch_metric(delta_F0, delta_F1, Tseg):
-    """
-    Calculate mismatch using the parameter space metric for F0 and F1.
-    
-    For F0 and F1, the metric components are:
-    g_00 = (π * T)² / 12  (for F0)
-    g_11 = (π * T²)² / 720  (for F1) 
-    g_01 = (π² * T³) / 120  (cross term)
-    """
-    # Metric components for F0-F1 parameter space
-    g_00 = (np.pi * Tseg)**2 / 12.0          # F0-F0 component
-    g_11 = (np.pi * Tseg**2)**2 / 720.0      # F1-F1 component  
-    g_01 = (np.pi**2 * Tseg**3) / 120.0      # F0-F1 cross component
-    
-    # Quadratic form: m = δλᵀ g δλ
-    mismatch = (g_00 * delta_F0**2 + 
-                g_11 * delta_F1**2 + 
-                2 * g_01 * delta_F0 * delta_F1)
-    
-    return mismatch, g_00, g_11, g_01
-
-def calculate_sky_mismatch_metric(delta_Alpha, delta_Delta, Tseg):
-    """
-    Calculate mismatch for sky position parameters using approximate metric.
-    """
-    # Simplified sky metric (this is approximate)
-    # For more precise calculations, use the full LALSuite metric functions
-    
-    # Typical scale for sky position metric
-    # This varies with detector motion and sky position
-    g_alpha = (2 * np.pi / (1 * 86400))**2 * Tseg**2  # Rough approximation
-    g_delta = g_alpha  # Simplified assumption
-    
-    mismatch = g_alpha * delta_Alpha**2 + g_delta * delta_Delta**2
-    
-    return mismatch, g_alpha, g_delta
-
-
-
-def plot_2F_scatter(res, label_name, xkey, ykey, labels, inj, outdir):
-    """Local plotting function to avoid code duplication in the 4D case"""
-    markersize = 1 if label_name == "grid" else 0.5
+def plot_2F_scatter(res, label, xkey, ykey):
+    """local plotting function to avoid code duplication in the 4D case"""
+    markersize = 1 if label == "grid" else 0.5
     plt.figure(figsize=(10, 6))
     sc = plt.scatter(res[xkey], res[ykey], c=res["twoF"], s=markersize)
     cb = plt.colorbar(sc)
-    plt.xlabel(labels[xkey])
-    plt.ylabel(labels[ykey])
-    cb.set_label(labels["2F"])
-    plt.title(label_name)
-    plt.plot(inj[xkey], inj[ykey], "*k", label="injection")
-    
+    plt.xlabel(config.labels[xkey])
+    plt.ylabel(config.labels[ykey])
+    cb.set_label(config.labels["2F"])
+    plt.title(label)
+    plt.plot(config.inj[xkey], config.inj[ykey], "*k", label="injection")
     maxidx = np.argmax(res["twoF"])
     plt.plot(
         res[xkey][maxidx],
         res[ykey][maxidx],
         "+r",
-        label=labels["max2F"],
+        label=config.labels["max2F"],
     )
     plt.legend(loc='upper right')
-    
-    plotfilename_base = os.path.join(
-        outdir, "{:s}_{:s}{:s}_2F".format(label_name, xkey, ykey)
-    )
+    plotfilename_base = os.path.join(outdir, "{:s}_{:s}{:s}_2F".format(label, xkey, ykey))
     plt.xlim([min(res[xkey]), max(res[xkey])])
     plt.ylim([min(res[ykey]), max(res[ykey])])
-    plt.savefig(plotfilename_base + ".pdf")
+    plt.savefig(plotfilename_base + ".png")
     # plt.show()
-
-
-
-class MismatchAnalyzer:
-    """
-    Class to analyze mismatch from PyFstat search results.
-    """
-    
-    def __init__(self, Tseg=None, duration=1.0, outdir='.'):
-        self.Tseg = Tseg
-        self.duration = duration  # Default duration if Tseg is not provided
-        self.outdir = outdir
-    
-    def analyze_grid_search_mismatch(self, gridsearch_obj, injection_params):
-        """
-        Comprehensive mismatch analysis for a PyFstat GridSearch object.
-        """
-        if not hasattr(gridsearch_obj, 'data'):
-            raise ValueError("GridSearch must be run first")
-        
-        # Get the best point from the search
-        max_point = gridsearch_obj.get_max_twoF()
-        
-        # Determine coherent time
-        Tseg = self.Tseg or self.duration
-        
-        # Calculate mismatch for each grid point
-        analysis = {}
-        
-        # Parameter offsets from injection
-        for param in ['F0', 'F1', 'Alpha', 'Delta']:
-            if param in gridsearch_obj.data.dtype.names and param in injection_params:
-                offsets = gridsearch_obj.data[param] - injection_params[param]
-                analysis[f'{param}_offsets'] = offsets
-        
-        # Mismatch calculations
-        mismatches = np.zeros(len(gridsearch_obj.data))
-        
-        # F0 contribution
-        if 'F0' in injection_params and 'F0' in gridsearch_obj.data.dtype.names:
-            F0_offsets = gridsearch_obj.data['F0'] - injection_params['F0']
-            F0_mm = (np.pi * Tseg * F0_offsets) ** 2 / 12.0
-            mismatches += F0_mm
-            analysis['F0_mismatch'] = F0_mm
-        
-        # F1 contribution  
-        if 'F1' in injection_params and 'F1' in gridsearch_obj.data.dtype.names:
-            F1_offsets = gridsearch_obj.data['F1'] - injection_params['F1']
-            F1_mm = (np.pi * Tseg**2 * F1_offsets) ** 2 / 720.0
-            mismatches += F1_mm
-            analysis['F1_mismatch'] = F1_mm
-        
-        # Sky position (simplified)
-        if all(p in injection_params and p in gridsearch_obj.data.dtype.names 
-               for p in ['Alpha', 'Delta']):
-            # Approximate sky mismatch
-            alpha_offset = gridsearch_obj.data['Alpha'] - injection_params['Alpha']
-            delta_offset = gridsearch_obj.data['Delta'] - injection_params['Delta']
-            
-            # Rough sky metric
-            sky_scale = (2 * np.pi / 86400)**2 * Tseg**2  
-            sky_mm = sky_scale * (alpha_offset**2 + delta_offset**2)
-            mismatches += sky_mm
-            analysis['sky_mismatch'] = sky_mm
-        
-        analysis['total_mismatch'] = mismatches
-        analysis['twoF_values'] = gridsearch_obj.data['twoF']
-        analysis['coherent_time'] = Tseg
-        analysis['injection_params'] = injection_params
-        analysis['max_point'] = max_point
-        
-        # Find best mismatch point
-        best_mm_idx = np.argmin(mismatches)
-        analysis['best_mismatch_point'] = {
-            'index': best_mm_idx,
-            'mismatch': mismatches[best_mm_idx],
-            'twoF': gridsearch_obj.data['twoF'][best_mm_idx],
-            'parameters': {param: gridsearch_obj.data[param][best_mm_idx] 
-                          for param in gridsearch_obj.data.dtype.names}
-        }
-        
-        # Find loudest point mismatch
-        loudest_idx = np.argmax(gridsearch_obj.data['twoF'])
-        analysis['loudest_point'] = {
-            'index': loudest_idx,
-            'mismatch': mismatches[loudest_idx],
-            'twoF': gridsearch_obj.data['twoF'][loudest_idx],
-            'parameters': {param: gridsearch_obj.data[param][loudest_idx] 
-                          for param in gridsearch_obj.data.dtype.names}
-        }
-        
-        return analysis
-    
-    def plot_mismatch_vs_twoF(self, analysis, savefig=True, filename=None):
-        """
-        Plot mismatch vs 2F to visualize the relationship.
-        """
-        if filename is None:
-            filename = os.path.join(self.outdir, "mismatch_vs_twoF_analysis.png")
-        
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6))
-        
-        # Plot 1: Mismatch vs 2F scatter
-        scatter = ax1.scatter(analysis['total_mismatch'], analysis['twoF_values'], 
-                           c=analysis['twoF_values'], cmap='viridis', alpha=0.7, s=20)
-        
-        # Highlight important points
-        loudest = analysis['loudest_point']
-        best_mm = analysis['best_mismatch_point']
-        
-        ax1.scatter(loudest['mismatch'], loudest['twoF'],
-                   c='red', s=100, marker='*', label='Loudest Point', zorder=5)
-        ax1.scatter(best_mm['mismatch'], best_mm['twoF'],
-                   c='orange', s=100, marker='s', label='Best Mismatch', zorder=5)
-        
-        ax1.set_xlabel('Mismatch')
-        ax1.set_ylabel('2F')
-        ax1.set_title('Mismatch vs 2F Distribution')
-        ax1.legend()
-        ax1.grid(True, alpha=0.3)
-        
-        plt.colorbar(scatter, ax=ax1, label='2F')
-        
-        # Plot 2: Mismatch histogram
-        ax2.hist(analysis['total_mismatch'], bins=50, alpha=0.7, edgecolor='black')
-        ax2.axvline(loudest['mismatch'], color='red', linestyle='--', 
-                   label=f'Loudest: {loudest["mismatch"]:.4f}')
-        ax2.axvline(best_mm['mismatch'], color='orange', linestyle='--',
-                   label=f'Best: {best_mm["mismatch"]:.4f}')
-        
-        ax2.set_xlabel('Mismatch')
-        ax2.set_ylabel('Count')
-        ax2.set_title('Mismatch Distribution')
-        ax2.legend()
-        ax2.grid(True, alpha=0.3)
-        
-        plt.tight_layout()
-        
-        if savefig:
-            plt.savefig(filename, dpi=150, bbox_inches='tight')
-            print(f"Mismatch analysis plot saved to: {filename}")
-        
-        # plt.show()
-        return fig, (ax1, ax2)
